88ff1b1fa3eba0df94a225610e951c9fbfe06147
IanAndrade2003 <ianandrade90@outlook.com>
2021-05-25 12:12:42 -0300

    Final

---
+++ index.js
@@ -0,0 +1,21 @@
+for(let numero = 1; numero <= 10; numero++ ){
+    console.log(numero)
+}
+
+let number = 10
+
+while(number >=1 ){
+    console.log(number)
+    number--
+}
+
+for(let n = 1; n<=100; n+=2){
+    console.log(n)
+}
+
+let n2 = 0
+
+while(n2 <=100){
+    console.log(n2)
+    n2+=2
+}
